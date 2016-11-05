7a3012b5a44e28d586e5ce27e4d5851b152ebfa3
PERSONAL\Miguel <ing.migueldiaz.2016@gmail.com>
2016-11-05 08:34:40 -0400

    Miguel D. Diaz H.

--- calculator.py
+++ calculator.py
@@ -1,16 +1,26 @@
+#! /usr/bin/env python2.7
 """
-Your module documentation here
+Module that determines sum total of the items in a list
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    This class contains only one method called "sum"
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Calculate the sum of the elements of a list
+
+		Inserts a list and will be calculate the sum of 
+		all elements of the list.
+
+		Parameters:
+		list -- List of numbers to sum
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        self.num_list = num_list
+		result = 0
+		for i in range(0,len(num_list)):
+			result = num_list[i] + result
+		return result
