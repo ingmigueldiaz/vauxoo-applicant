53f228cbbd09af3f464dad2f5eb563fa2586d18a
PERSONAL\Miguel <ing.migueldiaz.2016@gmail.com>
2016-11-05 08:54:56 -0400

    no message

--- primes.py
+++ primes.py
@@ -7,17 +7,18 @@ Module that determines whether a number is prime
 class PrimeClass(object):
     """
     This class contains only one method called "is_prime"
+    
     """
 
     def is_prime(self, num_int):
         """ 
-    	Calculate if a number is a prime
+    	Calculate if a number is a prime.
 
-		A prime number is a number greater than 1 has no positive
+
+		A prime number is a number greater than 1 has no positive 
 		divisors other than 1 and itself
 
-		Parameters:
-		num_int -- integer positive number
+
         """
         self.num_int = num_int
 		if num_int < 2 and num_int > 0:-
