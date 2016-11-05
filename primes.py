2695d02430acd1d71a4b521b56501ee055bcbad8
PERSONAL\Miguel <ing.migueldiaz.2016@gmail.com>
2016-11-05 08:35:37 -0400

    no message

--- primes.py
+++ primes.py
@@ -1,16 +1,29 @@
+#! /usr/bin/env python2.7
 """
-Your module documentation here
+Module that determines whether a number is prime
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    This class contains only one method called "is_prime"
     """
 
     def is_prime(self, num_int):
+        """ 
+    	Calculate if a number is a prime
+
+		A prime number is a number greater than 1 has no positive
+		divisors other than 1 and itself
+
+		Parameters:
+		num_int -- integer positive number
         """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        self.num_int = num_int
+		if num_int < 2 and num_int > 0:-
+			return False
+		for i in range(2,num_int):
+			if num_int%i == 0:
+				return False
+		return True
+        
